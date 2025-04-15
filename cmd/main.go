5cc655b59d633ef06e280ff6e0d59808136c75f9
luizpaulo73 <luiz.paulo2005@hotmail.com>
2025-04-14 23:23:53 -0300

    resolvendo problemas de cors

--- cmd/main.go
+++ cmd/main.go
@@ -7,11 +7,17 @@ import (
 	"github.com/luizpaulo73/usecase"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
 
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+	}))
 
 	dbConnection, err := db.ConnectDB()
 	if err != nil {
